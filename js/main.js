140c94721eed34ec8b9350e64aa28727b71646e0
jonahkula <jonahkula@gmail.com>
2021-02-10 17:54:59 -0800

    navbar setup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function navigateLink(url) {
   window.open(url);
 }
+
+Array.prototype.slice
+  .call(document.querySelectorAll(".navbar-burger"), 0)
+  .forEach((b) => {
+    b.addEventListener("click", () => {
+      let target = document.getElementById(b.dataset.target);
+      b.classList.toggle("is-active");
+      target.classList.toggle("is-active");
+    });
+  });
